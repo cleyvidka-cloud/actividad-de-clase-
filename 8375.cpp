e40e033d0f8a53598125a2aad6683e717938a10f
cleyvidka-cloud <cleyvidka@gmail.com>
2026-09-25 11:35:08 -0500

    Update 8375.cpp

--- 8375.cpp
+++ 8375.cpp
@@ -1,25 +1,19 @@
 #include <iostream>
-
 using namespace std;
-
 int main() {
-    // Optimización de entrada/salida para omegaUp
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-
-    int A, B;
-    if (cin >> A >> B) {
-        // Si las ganancias (A) superan las perdidas (B)
-        if (A > B) {
-            cout << "El negocio si va a jalar\n";
-            cout << (A - B) << "\n"; // Diferencia a favor de las ganancias
-        } 
-        // Si las perdidas (B) son mas grandes que las ganancias (A)
-        else {
-            cout << "Dejalo ya esta muerto\n";
-            cout << (B - A) << "\n"; // Diferencia a favor de las perdidas
-        }
-    }
-
-    return 0;
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+int A, B;
+if (cin >> A >> B) {
+if (A > B) {
+cout << "el negocio si va a jalar\n";
+cout << (A - B) << "\n"; 
+} 
+else {
+cout << "dejalo ya esta muerto\n";
+cout << (B - A) << "\n";
 }
+}
+return 0;
+}
+
