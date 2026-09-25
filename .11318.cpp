52e74fe71853da80fea90d070d0e5635a207b0b8
cleyvidka-cloud <cleyvidka@gmail.com>
2026-09-25 11:48:54 -0500

    Update .11318.cpp

--- .11318.cpp
+++ .11318.cpp
@@ -1,29 +1,22 @@
 #include <iostream>
-
 using namespace std;
-
 int main() {
-    // Optimización de entrada/salida para programación competitiva
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-
-    int A, B;
-    if (cin >> A >> B) {
-        // Primera condición: ¿A + B es igual a 5?
-        if (A + B == 5) {
-            B = B + 3; // Súmale 3 a B
-            cout << (2 * A + B) << "\n"; // Imprime 2A + B
-        } else {
-            A = A - 1; // Réstale 1 a A
-            
-            // Segunda condición: ¿7A + B es par?
-            if ((7 * A + B) % 2 == 0) {
-                cout << (A - B) << "\n"; // Imprime A - B
-            } else {
-                cout << (A * B) << "\n"; // Imprime A x B
-            }
-        }
-    }
-
-    return 0;
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+int a, b;
+if (cin >> a >> b) {
+if (a + b == 5) {
+b = b + 3;
+cout << (2 * a + b) << "\n";
+} else {
+a = a - 1;
+if ((7 * a + b) % 2 == 0) {
+cout << (a - b) << "\n";
+} else {
+cout << (a * b) << "\n";
 }
+}
+}
+return 0;
+}
+
